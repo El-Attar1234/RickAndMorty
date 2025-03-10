0da6cb6f20fa1a92b5cc20bc9bd7c9c50e7e2cc7
El-Attar1234 <mahmoudelattar427@gmail.com>
2025-03-10 16:10:25 +0200

    Add AppCoordinator and AppDependencyProvider

---
+++ RickAndMorty/Features/CharactersList/View/CharactersListView.swift
@@ -0,0 +1,18 @@
+//
+//  CharactersListView.swift
+//  RickAndMorty
+//
+//  Created by Mahmoud Elattar on 10/03/2025.
+//
+
+import SwiftUI
+
+struct CharactersListView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    CharactersListView()
+}
