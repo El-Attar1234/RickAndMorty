04a34d112b86f9260e75229e8961744111175bb0
El-Attar1234 <mahmoudelattar427@gmail.com>
2025-03-10 15:01:04 +0200

    Launch the project with adding gitignore file

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Mahmoud Elattar on 10/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
