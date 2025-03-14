b8181e354ed47cbb88d3e561d96c8103bab1458d
El-Attar1234 <mahmoudelattar427@gmail.com>
2025-03-14 03:14:09 +0200

    add CharactersTableViewRepresentable and CharactersTableViewController

---
+++ RickAndMorty/Features/CharactersList/View/CharactersListView/CharactersTableViewRepresentable.swift
@@ -0,0 +1,27 @@
+//
+//  CharactersTableViewRepresentable.swift
+//  RickAndMorty
+//
+//  Created by Mahmoud Elattar on 14/03/2025.
+//
+import SwiftUI
+
+struct CharactersTableViewRepresentable<ViewModel: CharactersViewModel>: UIViewControllerRepresentable {
+    @ObservedObject var viewModel: ViewModel
+    
+    func makeUIViewController(context: Context) -> CharactersTableViewController<ViewModel> {
+        let controller = CharactersTableViewController(viewModel: viewModel)
+        return controller
+    }
+    
+    func updateUIViewController(
+        _ uiViewController: CharactersTableViewController<ViewModel>,
+        context: Context
+    ) {
+        DispatchQueue.main.async {
+          //  if uiViewController.view.window != nil {
+                uiViewController.update(with: viewModel)
+            //}
+        }
+    }
+}
