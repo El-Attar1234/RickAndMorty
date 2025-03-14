81ef33ef4ca735fac5cd91a31c1659c08b7c3b48
El-Attar1234 <mahmoudelattar427@gmail.com>
2025-03-14 03:16:03 +0200

    create Dependencies

---
+++ RickAndMorty/Utils/Extensions/ UITableView+Ext.swift
@@ -0,0 +1,27 @@
+//
+//   UITableView+Ext.swift
+//  RickAndMorty
+//
+//  Created by Mahmoud Elattar on 14/03/2025.
+//
+import UIKit
+
+public extension UITableView {
+    
+    func register<T: UITableViewCell>(
+        cellType: T.Type,
+        bundle: Bundle? = nil
+    ) {
+        register(cellType, forCellReuseIdentifier: cellType.className)
+    }
+    
+    func dequeueReusableCell<T: UITableViewCell>(
+        with type: T.Type,
+        for indexPath: IndexPath
+    ) -> T {
+        guard let cell = dequeueReusableCell(withIdentifier: type.className, for: indexPath) as? T  else {
+            fatalError("Can't dequeue cell")
+        }
+        return cell
+    }
+}
